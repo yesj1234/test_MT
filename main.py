bd8996695c6dbe18018fd40a91e6109617ab28f7
yesj1234 <juesc007@gmail.com>
2024-02-06 06:47:56 +0000

    update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import sacrebleu
 import logging
 import sys 
 import evaluate
+import pandas as pd 
+import numpy as np
 from tqdm import tqdm 
 logger = logging.getLogger("MainLogger")
 logging.basicConfig(
@@ -17,8 +19,8 @@ logging.basicConfig(
 
 def main(args):
     #1. load the dataset. flores 200 by default. 
-    ds = load_dataset('facebook/flores', f'{args.source_lang}-{args.target_lang}',split='dev', trust_remote_code=True)
-
+    ds = load_dataset(args.dataset_name, f'{args.source_lang}-{args.target_lang}',split='dev', trust_remote_code=True)
+    df = ds.to_pandas()
     #2. load the model, config, tokenizer
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     tokenizer = AutoTokenizer.from_pretrained(args.model)
@@ -34,24 +36,32 @@ def main(args):
         prediction = tokenizer.decode(outputs[0], skip_special_tokens=True)
         predictions.append(prediction)
         references.append([data[f'sentence_{args.target_lang}']])
-    
+    assert len(predictions) == len(ds), f'length of predictions({len(predictions)}) does not match the length of ds({len(ds)})'
+    assert len(transcriptions) == len(ds), f'length of predictions({len(transcriptions)}) does not match the length of ds({len(ds)})'
+    # save the predictions in pd.DataFrame.
+    DF_FILENAME = f'{args.model.replace("/", "_")}_{args.dataset_name.replace("/","_")}.csv'
+    df.insert(len(ds), f'{args.model.replace("/", "_")}_prediction', predictions, True)
+    df.to_csv(DF_FILENAME)
     #4. compute the metrics 
     metric = evaluate.load('sacrebleu')
     score_file_name = f"./{args.dataset_name.replace('/', '_')}_{args.model.replace('/','_')}_scores.txt" 
+    scores = []
     with open(score_file_name, mode="w", encoding='utf-8') as f:
         for pred, ref in zip(predictions, references):
             score = metric.compute(predictions=[pred], references=ref)['score']
             score = round(score, 6)
+            scores.append(score)
             f.write(f'{pred} :: {ref[0]} :: {score}\n')
-
+    df.insert(len(ds), 'score', scores, True)
+    df.to_csv(DF_FILENAME)
     scores = 0
-    with open(f'./{args.model}_{args.dataset_name}_score.txt', mode="r", encoding='utf-8') as g:
+    with open(score_file_name, mode="r", encoding='utf-8') as g:
         lines = g.readlines()
         for line in lines:
             _, _, score = line.split(" :: ")
             score = round(float(score[:-2]), 2)
-            scores += score 
-        logger.info(f"BLEU: {scores/len(lines)}")
+            scores.append(score) 
+        logger.info(f"BLEU: {sum(scores)/len(lines)}")
             
             
             
